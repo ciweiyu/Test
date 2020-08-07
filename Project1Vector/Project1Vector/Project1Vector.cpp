27eb2117ca95faa4c2e186456e89d01cf0831057
wenyuchen <wenyuchen@bigo.sg>
2020-08-07 11:15:50 +0800

    update reverse

--- Project1Vector/Project1Vector/Project1Vector.cpp
+++ Project1Vector/Project1Vector/Project1Vector.cpp
@@ -4,6 +4,34 @@
 #include "stdafx.h"
 #include "..\..\common\VectorOperation.h"
 #include <vector>
+bool reverseCut(std::vector<int>& vecOrg, std::vector<int>& vecNew)
+{
+	std::vector<int>::reverse_iterator riter = vecOrg.rbegin();
+
+
+	int v = *riter;
+	std::cout << "last value=" << v << std::endl;
+	for (; riter !=vecOrg.rend(); riter++)
+	{
+		if (*riter * v <=0)
+		{
+			std::cout << "break at value=" << *riter << std::endl;
+			break;
+		}
+	}
+
+	// Reverse iterators have a member base() which returns a corresponding forward iterator. 
+	// Beware that this isn't an iterator that refers to the same object - it actually refers to the next object in the sequence.
+	// This is so that rbegin() corresponds with end() and rend() corresponds with begin().
+	// see https://en.cppreference.com/w/cpp/iterator/reverse_iterator
+	std::vector<int>::iterator iter = (riter + 1).base();
+
+	vecNew.clear();
+	vecNew.insert(vecNew.end(), iter, vecOrg.end());
+
+	vecOrg.erase(iter, vecOrg.end());
+	return true;
+}
 
 bool CutVec(std::vector<int>& vecOrg, std::vector<int>& vecNew)
 {
@@ -32,7 +60,7 @@ bool AscendingSort(std::vector<int>& vecTo)
 }
 int main()
 {
-	std::vector<int> originalVec= VectorOperation::GenerateVector(10,2,30);
+	std::vector<int> originalVec= VectorOperation::GenerateVector(10,-20,20);
 	VectorOperation::PrintVector("original vector", originalVec);
 	std::vector<int> newVec;
 
@@ -46,6 +74,19 @@ int main()
 	CutVec(originalVec, newVec);
 	VectorOperation::PrintVector("cut original vector", originalVec);
 	VectorOperation::PrintVector("cut new vector", newVec);
+
+	
+	for (int i = 0; i < 5; i++)
+	{
+		srand(i);
+		std::cout <<std::endl<< "reverse cut:" << i << std::endl;
+		originalVec = VectorOperation::GenerateVector(10, -20, 20);
+		VectorOperation::PrintVector("original vector", originalVec);
+		reverseCut(originalVec, newVec);
+		VectorOperation::PrintVector("cut original vector", originalVec);
+		VectorOperation::PrintVector("cut new vector", newVec);
+
+	}
 
 	getchar();
     return 0;
